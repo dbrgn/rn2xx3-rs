fe9ce5119d09a1487b64973857c5ab2f8ecb8c19
Danilo Bargen <mail@dbrgn.ch>
2020-03-06 12:09:27 +0100

    Expose error module

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,8 @@
+//! Error types used in this driver.
+
 use core::str::Utf8Error;
 
-/// A collection of all errors that can occur.
+/// A collection of errors that can occur.
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     /// Could not read from serial port.
